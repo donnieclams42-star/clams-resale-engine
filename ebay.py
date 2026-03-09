f8e30ac2ca4e68be27d5f0d5a37b7ed9a63710d5
Donald <donnieclams42@gmail.com>
2026-03-09 19:24:59 -0400

    fix ebay rate limit with token cache

--- ebay.py
+++ ebay.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 
 EBAY_CLIENT_ID = os.getenv("EBAY_CLIENT_ID")
 EBAY_CLIENT_SECRET = os.getenv("EBAY_CLIENT_SECRET")
@@ -7,14 +8,22 @@ EBAY_CLIENT_SECRET = os.getenv("EBAY_CLIENT_SECRET")
 TOKEN_URL = "https://api.ebay.com/identity/v1/oauth2/token"
 SEARCH_URL = "https://api.ebay.com/buy/browse/v1/item_summary/search"
 
+ACCESS_TOKEN = None
+TOKEN_EXPIRES = 0
+
 
 def get_token():
 
+    global ACCESS_TOKEN
+    global TOKEN_EXPIRES
+
+    # reuse token if still valid
+    if ACCESS_TOKEN and time.time() < TOKEN_EXPIRES:
+        return ACCESS_TOKEN
+
     response = requests.post(
         TOKEN_URL,
-        headers={
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
         data={
             "grant_type": "client_credentials",
             "scope": "https://api.ebay.com/oauth/api_scope"
@@ -26,7 +35,13 @@ def get_token():
 
     print("TOKEN RESPONSE:", data)
 
-    return data.get("access_token")
+    ACCESS_TOKEN = data.get("access_token")
+
+    expires_in = data.get("expires_in", 7200)
+
+    TOKEN_EXPIRES = time.time() + expires_in - 60
+
+    return ACCESS_TOKEN
 
 
 def get_market_data(query):
